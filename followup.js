9258e24849f00f8a3bd6f3afb5e7d84040f6fa65
Angel Solano <angelsolano@MacBook-Air.local>
2025-12-14 13:27:44 -0600

    added flavicon + new color

--- followup.js
+++ followup.js
@@ -1,9 +1,13 @@
 const numberInput = document.getElementById('contactnumber');
-const textArea = document.getElementById('CleanContactNumber');
 const contactButton = document.getElementById('ContactButton');
 
 numberInput.addEventListener('input', () => {
   const cleanedNumber = numberInput.value.replace(/\s+/g, '');
-//   textArea.value = cleanedNumber;
   contactButton.href = `https://wa.me/52${cleanedNumber}`;
+});
+
+contactButton.addEventListener('click', () => {
+  setTimeout(() => {
+    numberInput.value = '';
+  }, 100);
 });

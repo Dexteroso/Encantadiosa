ce390706066e87f508ddfed5bbb7ba3663ed9dfb
Angel Solano <angelsolano@MacBook-Air.local>
2025-12-14 14:04:45 -0600

    added message field

--- followup.js
+++ followup.js
@@ -1,13 +1,14 @@
 const numberInput = document.getElementById('contactnumber');
 const contactButton = document.getElementById('ContactButton');
+const messageContent = document.getElementById('message');
 
-numberInput.addEventListener('input', () => {
-  const cleanedNumber = numberInput.value.replace(/\s+/g, '');
-  contactButton.href = `https://wa.me/52${cleanedNumber}`;
-});
+numberInput.addEventListener('input', updateWhatsAppLink);
+messageContent.addEventListener('input', updateWhatsAppLink);
 
-contactButton.addEventListener('click', () => {
-  setTimeout(() => {
-    numberInput.value = '';
-  }, 100);
-});
+function updateWhatsAppLink() {
+  const cleanedNumber = numberInput.value.replace(/\D/g, '');
+  const encodedMessage = encodeURIComponent(messageContent.value);
+
+  contactButton.href =
+    `https://api.whatsapp.com/send/?phone=52${cleanedNumber}&text=${encodedMessage}&app_absent=0`;
+}
